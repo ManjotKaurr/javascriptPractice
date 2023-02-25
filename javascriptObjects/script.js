2a6a28331076cc493235dace12a165dfccd9a87e
manjotkaurr <manjotkaurncc@gmail.com>
2023-02-25 17:33:05 +0530

    initial commit

---
+++ javascriptObjects/script.js
@@ -0,0 +1,24 @@
+const penStand = {
+    material:'plastic',
+    color:'brown',
+    itemsCapacity: 5,
+    weight:'200gms',
+    measurements : {
+        length:20,
+        breadth: 50,
+    },
+    hasClock:false,
+};
+const table = {
+    material:'wood',
+    measurements:{
+        length:150,
+        breadth:100,
+        legs:'four',
+        standHeight:350,
+    },
+    hasLocker:false,
+    hasFootstand:true,
+
+};
+// console.log(penStand);
